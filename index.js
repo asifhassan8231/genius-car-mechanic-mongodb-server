96add2a6b9e2cbecabc4f6725ba89085560c3545
asifhassan8231 <asifhassan8231@gmail.com>
2021-10-26 17:19:25 +0600

    first

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const port = 5000;
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yb4zx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("geniusCarMechanic");
+        const serviceCollection = database.collection("services");
+
+        //insert a new service POST API
+        app.post('/services', async (req, res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.json(result);
+        })
+
+        //get all services from db
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello 5000!')
+})
+
+app.listen(port, () => {
+    console.log('listening to', port);
+})
+
+
+
+
+
+/*
+user:mongouser1
+password:BOac9Oa1JG5xBs0q
+*/
