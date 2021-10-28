fb68be177ca4338bf11a4678be0f0ca9d40456d7
asifhassan8231 <asifhassan8231@gmail.com>
2021-10-28 16:10:54 +0600

    added port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 //middleware
